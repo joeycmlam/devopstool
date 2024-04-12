d71ef583a31718a38921756208a6f1f14eed529e
joeylam <joey.cm.lam@gmail.com>
2024-04-12 22:44:07 +0800

    remove unnecessary files

--- src/lib/configDatabase.ts
+++
@@ -1,9 +0,0 @@
-import { BaseConfig } from "./configHelper";
-
-export default interface DatabaseConfig extends BaseConfig {
-    host: string;
-    port: number;
-    user: string;
-    password: string;
-    database: string;
-}
