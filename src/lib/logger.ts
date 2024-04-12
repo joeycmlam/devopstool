41bc3c8d0a41d8225a41bb195b00c01291195fa2
joeylam <joey.cm.lam@gmail.com>
2024-04-12 23:51:43 +0800

    reflector

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -12,19 +12,7 @@ export default class Logger {
   }
 
   static {
-    let config: { filename?: string, level?: string };
-    const configFile = path.resolve(require.main!.path!, './config.logger.yaml');
-
-    try {
-      const configData = fs.readFileSync(configFile, 'utf-8');
-      config = yaml.load(configData) as { filename?: string, level?: string };
-    } catch (err) {
-      // Default configuration
-      config = {
-        filename: path.basename('app'),
-        level: 'info'
-      };
-    }
+    let config: { filename?: string, level?: string } = { filename: path.basename('app'), level: 'info' };
 
     const logFilePattern = 'yyyyMMdd.log';
     const fileName = config.filename || 'app';
