8aa6e7008ab51f55a41d9e4a882f61a616578d87
joeylam <joey.cm.lam@gmail.com>
2024-04-13 02:09:30 +0800

    update test framework

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    collectCoverageFrom: [
-        "src/app/*/*.{ts}",
-        "!src/app/*/test/features/step_definitions/*.{ts}",
-        "!test/*/"
-      ]
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['src/*/test/jest.*.ts'],
+  collectCoverageFrom: [
+      "src/*/test/*.ts",
+      "!src/app/*/test/features/step_definitions/*.{ts}",
+      "!test/*/"
+  ]
 };
